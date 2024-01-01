283936991696b36a756406ecf7323a653d255791
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-01-02 00:00:00 +0530

    Added JavaScript practice question - Day 2

--- js-practice/day2.js
+++ js-practice/day2.js
@@ -4,3 +4,6 @@ for(let i=0; i<5; i++) { console.log(i); }
 
 // Question: While Loop
 let i = 0; while(i < 5) { console.log(i); i++; }
+
+// Question: Functions
+function greet() { return 'Hello, World!'; } console.log(greet());
