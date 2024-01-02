79eb8869bf4b3ba0b2c8904d01688a880a8e2cce
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-01-03 00:00:00 +0530

    Added JavaScript practice question - Day 3

--- js-practice/day3.js
+++ js-practice/day3.js
@@ -4,3 +4,6 @@ let user = {name: 'Alice', age: 25}; console.log(user.name);
 
 // Question: DOM Manipulation
 document.getElementById('myId').innerText = 'Updated';
+
+// Question: Event Listeners
+document.addEventListener('click', () => console.log('Clicked!'));
