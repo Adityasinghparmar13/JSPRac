2099dad768afa93b96df21d49a228e5d60b0516f
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-01-03 00:00:00 +0530

    Added JavaScript practice question - Day 3

--- js-practice/day3.js
+++ js-practice/day3.js
@@ -1,3 +1,6 @@
 
 // Question: Object Properties
 let user = {name: 'Alice', age: 25}; console.log(user.name);
+
+// Question: DOM Manipulation
+document.getElementById('myId').innerText = 'Updated';
