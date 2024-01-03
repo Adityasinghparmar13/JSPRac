0335b2da8b7d9dea2b8b6558e7bcc6bfbdf7f7af
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-01-04 00:00:00 +0530

    Added JavaScript practice question - Day 4

--- js-practice/day4.js
+++ js-practice/day4.js
@@ -1,3 +1,6 @@
 
 // Question: JSON Parsing
 let obj = JSON.parse('{"name": "John"}'); console.log(obj.name);
+
+// Question: Fetch API
+fetch('https://api.example.com').then(res => res.json()).then(data => console.log(data));
