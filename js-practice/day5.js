da85605fd12526ed10f3e91b67aa358a204c1ce6
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-01-05 00:00:00 +0530

    Added JavaScript practice question - Day 5

--- js-practice/day5.js
+++ js-practice/day5.js
@@ -1,3 +1,6 @@
 
 // Question: Closures
 function outer() { let count = 0; return () => count++; } let counter = outer();
+
+// Question: Lexical Scope
+let a = 'Hello'; function say() { console.log(a); }
