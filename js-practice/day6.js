12737ec1f0721b19056845da3dc509806ae04583
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-01-06 00:00:00 +0530

    Added JavaScript practice question - Day 6

--- js-practice/day6.js
+++ js-practice/day6.js
@@ -1,3 +1,6 @@
 
 // Question: Spread Operator
 let arr1 = [1,2]; let arr2 = [...arr1,3,4]; console.log(arr2);
+
+// Question: Rest Parameters
+function sum(...nums) { return nums.reduce((a,b) => a+b); }
