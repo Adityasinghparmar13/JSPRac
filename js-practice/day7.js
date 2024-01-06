fdd8fa1a6f8bd681de6556d80384694b9a82f319
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-01-07 00:00:00 +0530

    Added JavaScript practice question - Day 7

--- js-practice/day7.js
+++ js-practice/day7.js
@@ -1,3 +1,6 @@
 
 // Question: WebSockets
 let ws = new WebSocket('ws://example.com'); ws.onmessage = event => console.log(event.data);
+
+// Question: Node.js File System
+const fs = require('fs'); fs.writeFileSync('test.txt', 'Hello');
