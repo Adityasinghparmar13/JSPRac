acabcc785da6c6a01fe24f1fa21d25a6f8120af8
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-03-01 00:00:00 +0530

    Added JavaScript practice question - Day 1

--- js-practice/day1.js
+++ js-practice/day1.js
@@ -10,3 +10,6 @@ let age = 18; if(age >= 18) console.log('Adult');
 
 // Question: Switch Case
 let day = 3; switch(day) { case 3: console.log('Wednesday'); break; }
+
+// Question: Print 'Hello World' to the console
+console.log('Hello World');
