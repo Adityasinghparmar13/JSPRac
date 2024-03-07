89899542eb585dbb5b8c8ef111b5bac7c77c9937
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-03-08 00:00:00 +0530

    Added JavaScript practice question - Day 8

--- js-practice/day8.js
+++ js-practice/day8.js
@@ -4,3 +4,6 @@ let [first, second] = [1,2,3]; console.log(first);
 
 // Question: Destructure object properties
 let {name} = person; console.log(name);
+
+// Question: Use ternary operator
+let isAdult = age >= 18 ? 'Yes' : 'No';
