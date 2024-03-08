fa6688d251806ec3c41ca4c31c6715b9bb7e4770
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-03-09 00:00:00 +0530

    Added JavaScript practice question - Day 9

--- js-practice/day9.js
+++ js-practice/day9.js
@@ -7,3 +7,10 @@ document.getElementById('header').textContent = 'New Title';
 
 // Question: Disable a button element
 document.querySelector('button').disabled = true;
+
+// Question: Implement array chunking function
+
+    function chunkArray(arr, size) {
+      return Array.from({length: Math.ceil(arr.length/size)}, (_,i) => 
+        arr.slice(i*size, i*size+size));
+    }
