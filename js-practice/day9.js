7a7388ea6c748bab8daa3b71a387a4031c5a7f57
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-03-09 00:00:00 +0530

    Added JavaScript practice question - Day 9

--- js-practice/day9.js
+++ js-practice/day9.js
@@ -1,3 +1,6 @@
 
 // Question: Create a new div element
 document.createElement('div');
+
+// Question: Change text of element with id 'header'
+document.getElementById('header').textContent = 'New Title';
