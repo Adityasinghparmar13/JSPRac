594e14030cc75d442b717c46f1bba45eec678121
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-03-10 00:00:00 +0530

    Added JavaScript practice question - Day 10

--- js-practice/day10.js
+++ js-practice/day10.js
@@ -18,3 +18,11 @@ let cloned = JSON.parse(JSON.stringify(original));
         return key in cache ? cache[key] : (cache[key] = fn(...args));
       };
     }
+
+// Question: Create observable with subscribers
+
+    class Observable {
+      constructor() { this.subscribers = []; }
+      subscribe(fn) { this.subscribers.push(fn); }
+      next(val) { this.subscribers.forEach(fn => fn(val)); }
+    }
