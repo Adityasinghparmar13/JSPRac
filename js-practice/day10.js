472ebb54b3292c4942fc6903a73cd91aaa536bfb
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-03-10 00:00:00 +0530

    Added JavaScript practice question - Day 10

--- js-practice/day10.js
+++ js-practice/day10.js
@@ -1,3 +1,10 @@
 
 // Question: Deep clone object using JSON
 let cloned = JSON.parse(JSON.stringify(original));
+
+// Question: Flatten nested array recursively
+
+    function flatten(arr) {
+      return arr.reduce((acc, val) => 
+        Array.isArray(val) ? acc.concat(flatten(val)) : acc.concat(val), []);
+    }
