d02a81daa28858d0d55afce0dc1c886506258982
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-03-12 00:00:00 +0530

    Added JavaScript practice question - Day 12

--- js-practice/day12.js
+++ js-practice/day12.js
@@ -25,3 +25,12 @@
     function isValidEmail(email) {
       return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
     }
+
+// Question: Create HOC for authentication
+
+    function withAuth(Component) {
+      return props => {
+        const isAuth = checkAuth();
+        return isAuth ? <Component {...props} /> : <Login />;
+      };
+    }
