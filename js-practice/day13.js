033052475b7dd6dc3beb69622b9009ca3f56a695
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-03-13 00:00:00 +0530

    Added JavaScript practice question - Day 13

--- js-practice/day13.js
+++ js-practice/day13.js
@@ -4,3 +4,11 @@
     const worker = new Worker('worker.js');
     worker.postMessage('Start');
     worker.onmessage = e => console.log(e.data);
+
+// Question: Upload file with progress
+
+    const xhr = new XMLHttpRequest();
+    xhr.upload.onprogress = e => 
+      console.log((e.loaded / e.total * 100).toFixed(0) + '%');
+    xhr.open('POST', '/upload');
+    xhr.send(formData);
