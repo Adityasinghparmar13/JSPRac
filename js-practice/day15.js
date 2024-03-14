852eafe3498e11c59d8f5c4ef09d86db3e3291ce
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-03-15 00:00:00 +0530

    Added JavaScript practice question - Day 15

--- js-practice/day15.js
+++ js-practice/day15.js
@@ -7,3 +7,14 @@
       console.log('New state:', store.getState());
       return result;
     };
+
+// Question: Create TypeScript decorator
+
+    function log(target, key, descriptor) {
+      const original = descriptor.value;
+      descriptor.value = function(...args) {
+        console.log('Called:', key);
+        return original.apply(this, args);
+      };
+      return descriptor;
+    }
