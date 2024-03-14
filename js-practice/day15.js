5b298fd968393a5f631855f7d91ce30f722927e1
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-03-15 00:00:00 +0530

    Added JavaScript practice question - Day 15

--- js-practice/day15.js
+++ js-practice/day15.js
@@ -18,3 +18,16 @@
       };
       return descriptor;
     }
+
+// Question: Build Webpack configuration
+
+    module.exports = {
+      entry: './src/index.js',
+      output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+      },
+      module: {
+        rules: [{ test: /\.js$/, use: 'babel-loader' }]
+      }
+    };
