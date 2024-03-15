6cd634e6e156841bbb7b9339291e65c2d6ea0e77
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-03-16 00:00:00 +0530

    Added JavaScript practice question - Day 16

--- js-practice/day16.js
+++ js-practice/day16.js
@@ -36,3 +36,16 @@
       };
       return response;
     };
+
+// Question: Implement OAuth2 flow
+
+    const oauth2 = require('simple-oauth2').create({
+      client: {
+        id: 'client_id',
+        secret: 'client_secret'
+      },
+      auth: {
+        tokenHost: 'https://api.oauth.com'
+      }
+    });
+    const token = await oauth2.clientCredentials.getToken();
