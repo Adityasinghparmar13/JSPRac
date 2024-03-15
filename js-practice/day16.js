ee3ea761e15c0efe251c9603e9df226261a42e60
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-03-16 00:00:00 +0530

    Added JavaScript practice question - Day 16

--- js-practice/day16.js
+++ js-practice/day16.js
@@ -26,3 +26,13 @@
         - uses: actions/checkout@v2
         - run: npm install
         - run: npm test
+
+// Question: Create AWS Lambda handler
+
+    exports.handler = async (event) => {
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+      };
+      return response;
+    };
