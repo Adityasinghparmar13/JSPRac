a18dcf7d0b28edf552ae0b97f91ded9546da8143
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-04-02 00:00:00 +0530

    Added JavaScript practice question - Day 2

--- js-practice/day2.js
+++ js-practice/day2.js
@@ -21,3 +21,13 @@ function reverseString(str) {
     return str.split('').reverse().join('');
 }
 console.log(reverseString('hello')); // 'olleh'
+
+// Question: 5. Calculate factorial
+function factorial(n) {
+    let result = 1;
+    for (let i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+console.log(factorial(5)); // 120
