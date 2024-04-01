3da4bea68b56958e4976aed92de385ea6f123bc5
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-04-02 00:00:00 +0530

    Added JavaScript practice question - Day 2

--- js-practice/day2.js
+++ js-practice/day2.js
@@ -34,3 +34,6 @@ fruits.forEach(fruit => console.log(fruit));
 
 // Question: Create an array of 3 fruits
 let fruits = ['apple', 'banana', 'orange'];
+
+// Question: Create a function to square a number
+function square(n) { return n * n; }
