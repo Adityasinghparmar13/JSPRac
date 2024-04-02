52145f98ae2fe512df37fe191a7337704fc35380
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-04-03 00:00:00 +0530

    Added JavaScript practice question - Day 3

--- js-practice/day3.js
+++ js-practice/day3.js
@@ -1,3 +1,6 @@
 
 // Question: Remove the last element of an array
 let arr = [1,2,3]; arr.pop(); console.log(arr);
+
+// Question: Check if a string contains 'hello'
+let str = 'hello world'; console.log(str.includes('hello'));
