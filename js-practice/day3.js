ca8d84ae68ab047d0587da8e741a54b029badb06
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-04-03 00:00:00 +0530

    Added JavaScript practice question - Day 3

--- js-practice/day3.js
+++ js-practice/day3.js
@@ -4,3 +4,9 @@ function findArrayMax(arr) {
     return Math.max(...arr);
 }
 console.log(findArrayMax([5, 2, 9, 1])); // 9
+
+// Question: 9. Calculate array sum
+function arraySum(arr) {
+    return arr.reduce((acc, val) => acc + val, 0);
+}
+console.log(arraySum([1, 2, 3, 4])); // 10
