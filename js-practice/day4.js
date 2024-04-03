ce0cc0ea04f3ed60201ddb59004548a683d70d49
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-04-04 00:00:00 +0530

    Added JavaScript practice question - Day 4

--- js-practice/day4.js
+++ js-practice/day4.js
@@ -4,3 +4,6 @@ let data = { user: { id: 1 } }; console.log(data.user.id);
 
 // Question: Merge two arrays using concat
 let arr1 = [1,2]; let arr2 = [3,4]; console.log(arr1.concat(arr2));
+
+// Question: Find index of 'banana' in array
+console.log(fruits.indexOf('banana'));
