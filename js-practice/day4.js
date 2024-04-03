3662b96dd80e06fbd047486bfd0f279be7374847
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-04-04 00:00:00 +0530

    Added JavaScript practice question - Day 4

--- js-practice/day4.js
+++ js-practice/day4.js
@@ -1,3 +1,6 @@
 
 // Question: Access nested object property
 let data = { user: { id: 1 } }; console.log(data.user.id);
+
+// Question: Merge two arrays using concat
+let arr1 = [1,2]; let arr2 = [3,4]; console.log(arr1.concat(arr2));
