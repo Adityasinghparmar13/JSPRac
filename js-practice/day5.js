f91e9ceeba68238bfd89a8eaccb117f56838cb1f
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-04-05 00:00:00 +0530

    Added JavaScript practice question - Day 5

--- js-practice/day5.js
+++ js-practice/day5.js
@@ -36,3 +36,12 @@ function sumEvenInRange(start, end) {
     return sum;
 }
 console.log(sumEvenInRange(1, 10)); // 30
+
+// Question: 18. Calculate rectangle area
+
+// Basic geometry calculation
+function rectangleArea(width, height) {
+    // Multiply dimensions
+    return width * height;
+}
+console.log(rectangleArea(5, 4)); // 20
