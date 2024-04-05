96ee5e4c3897fb8fa079a206ee6c12938d8831e4
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-04-06 00:00:00 +0530

    Added JavaScript practice question - Day 6

--- js-practice/day6.js
+++ js-practice/day6.js
@@ -37,3 +37,6 @@ console.log(new Date().getFullYear());
 
 // Question: Find maximum value in array
 console.log(Math.max(...nums));
+
+// Question: Filter even numbers from array
+console.log(nums.filter(n => n%2 === 0));
