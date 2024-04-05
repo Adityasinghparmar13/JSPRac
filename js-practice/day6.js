84175fc52a42ab6b9c88c9b0ceca58e13acfd8c1
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-04-06 00:00:00 +0530

    Added JavaScript practice question - Day 6

--- js-practice/day6.js
+++ js-practice/day6.js
@@ -19,3 +19,12 @@ function toTitleCase(str) {
     ).join(' ');
 }
 console.log(toTitleCase('hello world')); // 'Hello World'
+
+// Question: 22. Replace character in string
+
+// Replace all instances of a character
+function replaceChar(str, oldChar, newChar) {
+    // Use split and join method
+    return str.split(oldChar).join(newChar);
+}
+console.log(replaceChar('hello', 'l', 'x')); // 'hexxo'
