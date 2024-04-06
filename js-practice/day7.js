d6b478c57b4b5b79662c3024ded62ee2bf8209f6
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-04-07 00:00:00 +0530

    Added JavaScript practice question - Day 7

--- js-practice/day7.js
+++ js-practice/day7.js
@@ -4,3 +4,6 @@ console.log(Math.max(...nums));
 
 // Question: Filter even numbers from array
 console.log(nums.filter(n => n%2 === 0));
+
+// Question: Create a Set from array
+let unique = new Set([1,1,2,3]); console.log([...unique]);
