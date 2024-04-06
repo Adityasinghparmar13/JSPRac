cf0c6947e1eaf39b4d46fb3580fdaee37887e1d1
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-04-07 00:00:00 +0530

    Added JavaScript practice question - Day 7

--- js-practice/day7.js
+++ js-practice/day7.js
@@ -18,3 +18,16 @@ function removeDuplicates(arr) {
     return [...new Set(arr)];
 }
 console.log(removeDuplicates([1,2,2,3])); // [1,2,3]
+
+// Question: 28. Reverse array elements
+
+// Manual array reversal
+function reverseArray(arr) {
+    const reversed = [];
+    // Iterate from end to start
+    for (let i = arr.length - 1; i >= 0; i--) {
+        reversed.push(arr[i]);
+    }
+    return reversed;
+}
+console.log(reverseArray([1,2,3])); // [3,2,1]
