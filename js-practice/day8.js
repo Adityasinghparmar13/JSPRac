503d1b1762b41f9fba08dccde3e7642b5a9dc786
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-04-08 00:00:00 +0530

    Added JavaScript practice question - Day 8

--- js-practice/day8.js
+++ js-practice/day8.js
@@ -30,3 +30,13 @@ function arrayAverage(arr) {
     return arr.reduce((a, b) => a + b, 0) / arr.length;
 }
 console.log(arrayAverage([4, 2, 8, 6])); // 5
+
+// Question: 30. Sum of digits in number
+
+// Convert number to string for processing
+function sumDigits(num) {
+    return String(num).split('').reduce(
+        (sum, digit) => sum + Number(digit), 0
+    );
+}
+console.log(sumDigits(123)); // 6
