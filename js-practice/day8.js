36a221d696f8c04fa847156ed17ba772e5b02e8c
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-04-08 00:00:00 +0530

    Added JavaScript practice question - Day 8

--- js-practice/day8.js
+++ js-practice/day8.js
@@ -22,3 +22,6 @@ let price = 9.99; console.log(price.toFixed(2));
     let promise = new Promise((resolve) => {
       setTimeout(() => resolve('Done'), 1000);
     });
+
+// Question: Use ternary operator
+let isAdult = age >= 18 ? 'Yes' : 'No';
