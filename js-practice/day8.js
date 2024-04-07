c70c94dfb3b87c3d679ff133c6086d03623c1a8d
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-04-08 00:00:00 +0530

    Added JavaScript practice question - Day 8

--- js-practice/day8.js
+++ js-practice/day8.js
@@ -16,3 +16,9 @@ let isAdult = age >= 18 ? 'Yes' : 'No';
 
 // Question: Format number to 2 decimal places
 let price = 9.99; console.log(price.toFixed(2));
+
+// Question: Create a promise that resolves
+
+    let promise = new Promise((resolve) => {
+      setTimeout(() => resolve('Done'), 1000);
+    });
