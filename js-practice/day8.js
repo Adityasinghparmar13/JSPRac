569fdd3d4a8c5a43a1eb09c3288c43328dea3ab9
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-04-08 00:00:00 +0530

    Added JavaScript practice question - Day 8

--- js-practice/day8.js
+++ js-practice/day8.js
@@ -12,3 +12,12 @@ function fizzBuzz(n) {
     }
 }
 fizzBuzz(5); // 1, 2, Fizz, 4, Buzz
+
+// Question: 29. Merge two arrays
+
+// Concatenate arrays without duplicates
+function mergeArrays(arr1, arr2) {
+    // Combine and filter duplicates
+    return [...new Set([...arr1, ...arr2])];
+}
+console.log(mergeArrays([1,2], [2,3])); // [1,2,3]
