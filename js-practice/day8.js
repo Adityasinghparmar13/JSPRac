cc3cdeb13d36e195ae0304b7a68d471a9fc1b1f0
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-04-08 00:00:00 +0530

    Added JavaScript practice question - Day 8

--- js-practice/day8.js
+++ js-practice/day8.js
@@ -1,3 +1,9 @@
 
 // Question: Check if object has property
 console.log('name' in person);
+
+// Question: Create a Map with key-value pairs
+
+    let map = new Map();
+    map.set('name', 'Alice');
+    console.log(map.get('name'));
