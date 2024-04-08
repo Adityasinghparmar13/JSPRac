718bf2746cfaf700405d4bf9f0a0081c69393ce9
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-04-09 00:00:00 +0530

    Added JavaScript practice question - Day 9

--- js-practice/day9.js
+++ js-practice/day9.js
@@ -30,3 +30,6 @@ document.querySelector('button').disabled = true;
 
 // Question: Create a new div element
 document.createElement('div');
+
+// Question: Change text of element with id 'header'
+document.getElementById('header').textContent = 'New Title';
