af44d44796b8d29bdfbe71c457a993386d2f02ab
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-04-10 00:00:00 +0530

    Added JavaScript practice question - Day 10

--- js-practice/day10.js
+++ js-practice/day10.js
@@ -16,3 +16,15 @@ function compactArray(arr) {
     return arr.filter(Boolean);
 }
 console.log(compactArray([0, 1, false, '', 3])); // [1, 3]
+
+// Question: 39. Find index of element in array
+
+// Custom indexOf implementation
+function findIndex(arr, element) {
+    // Loop through array elements
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === element) return i;
+    }
+    return -1;
+}
+console.log(findIndex(['a', 'b', 'c'], 'b')); // 1
