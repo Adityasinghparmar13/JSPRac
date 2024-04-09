61f1af8263d1366ca3c040481ae3f245b754da1e
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-04-10 00:00:00 +0530

    Added JavaScript practice question - Day 10

--- js-practice/day10.js
+++ js-practice/day10.js
@@ -7,3 +7,12 @@ function allPositive(numbers) {
     return numbers.every(num => num > 0);
 }
 console.log(allPositive([5, 3, 8])); // true
+
+// Question: 40. Remove falsy values from array
+
+// Filter out falsy values
+function compactArray(arr) {
+    // Use Boolean constructor as filter
+    return arr.filter(Boolean);
+}
+console.log(compactArray([0, 1, false, '', 3])); // [1, 3]
