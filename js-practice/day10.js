3ec544a4fe083793559acc4e32004f1c34d1da1a
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-04-10 00:00:00 +0530

    Added JavaScript practice question - Day 10

--- js-practice/day10.js
+++ js-practice/day10.js
@@ -1,3 +1,10 @@
 
 // Question: Create a new div element
 document.createElement('div');
+
+// Question: Implement array chunking function
+
+    function chunkArray(arr, size) {
+      return Array.from({length: Math.ceil(arr.length/size)}, (_,i) => 
+        arr.slice(i*size, i*size+size));
+    }
