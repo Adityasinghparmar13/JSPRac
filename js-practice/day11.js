a2763f014eb863080da9e12a917aee64e0d770d6
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-04-11 00:00:00 +0530

    Added JavaScript practice question - Day 11

--- js-practice/day11.js
+++ js-practice/day11.js
@@ -5,3 +5,13 @@
       return arr.reduce((acc, val) => 
         Array.isArray(val) ? acc.concat(flatten(val)) : acc.concat(val), []);
     }
+
+// Question: Memoize function results
+
+    function memoize(fn) {
+      let cache = {};
+      return (...args) => {
+        let key = JSON.stringify(args);
+        return key in cache ? cache[key] : (cache[key] = fn(...args));
+      };
+    }
