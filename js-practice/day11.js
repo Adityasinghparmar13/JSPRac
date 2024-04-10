6d312582ec36a6227dbcc43444589c4b6484ef5b
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-04-11 00:00:00 +0530

    Added JavaScript practice question - Day 11

--- js-practice/day11.js
+++ js-practice/day11.js
@@ -59,3 +59,8 @@
       return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
         (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
     }
+
+// Question: Parse query parameters from URL
+
+    let params = new URLSearchParams(window.location.search);
+    console.log(params.get('page'));
