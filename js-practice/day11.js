5abb28b6f034cb4db2caad19c2e0bb5e9acb93d8
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-04-11 00:00:00 +0530

    Added JavaScript practice question - Day 11

---
+++ js-practice/day11.js
@@ -0,0 +1,11 @@
+
+// Question: 42. Flatten nested arrays
+
+// Recursive flattening using reduce
+function flattenArray(arr) {
+    return arr.reduce((acc, item) => {
+        // Handle nested arrays recursively
+        return acc.concat(Array.isArray(item) ? flattenArray(item) : item);
+    }, []);
+}
+console.log(flattenArray([1,[2,[3]]])); // [1,2,3]
