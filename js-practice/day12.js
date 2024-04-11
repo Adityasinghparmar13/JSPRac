7ac50b1894d6f650dc13e08c70715f08e4378280
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-04-12 00:00:00 +0530

    Added JavaScript practice question - Day 12

--- js-practice/day12.js
+++ js-practice/day12.js
@@ -87,3 +87,12 @@
         }
       };
     }
+
+// Question: Create HOC for authentication
+
+    function withAuth(Component) {
+      return props => {
+        const isAuth = checkAuth();
+        return isAuth ? <Component {...props} /> : <Login />;
+      };
+    }
