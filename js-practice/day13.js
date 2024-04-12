82590c9dd6f1df11f820a5933ae079e66c7827a5
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-04-13 00:00:00 +0530

    Added JavaScript practice question - Day 13

--- js-practice/day13.js
+++ js-practice/day13.js
@@ -8,3 +8,14 @@ function sumOfSquares(numbers) {
     );
 }
 console.log(sumOfSquares([2, 3])); // 13
+
+// Question: 50. Find longest word
+
+// Split and reduce to find maximum
+function longestWord(str) {
+    return str.split(/\s+/).reduce((longest, word) => 
+        // Compare word lengths
+        word.length > longest.length ? word : longest
+    , '');
+}
+console.log(longestWord('The quick brown fox')); // 'quick'
