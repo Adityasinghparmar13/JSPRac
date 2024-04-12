c1fb0d88a17367756dc05b544d1bf5e7c681f79c
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-04-13 00:00:00 +0530

    Added JavaScript practice question - Day 13

--- js-practice/day13.js
+++ js-practice/day13.js
@@ -72,3 +72,25 @@
       idx++;
       return [state, setState];
     }
+
+// Question: Implement LRU cache
+
+    class LRUCache {
+      constructor(capacity) {
+        this.cache = new Map();
+        this.capacity = capacity;
+      }
+      get(key) {
+        if(!this.cache.has(key)) return -1;
+        const val = this.cache.get(key);
+        this.cache.delete(key);
+        this.cache.set(key, val);
+        return val;
+      }
+      put(key, value) {
+        if(this.cache.has(key)) this.cache.delete(key);
+        this.cache.set(key, value);
+        if(this.cache.size > this.capacity) 
+          this.cache.delete(this.cache.keys().next().value);
+      }
+    }
