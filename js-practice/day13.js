d05bb88b61ec0a16796ca528beb634c22f7b7f10
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-04-13 00:00:00 +0530

    Added JavaScript practice question - Day 13

--- js-practice/day13.js
+++ js-practice/day13.js
@@ -29,3 +29,12 @@ function areAnagrams(str1, str2) {
     return normalize(str1) === normalize(str2);
 }
 console.log(areAnagrams('listen', 'silent')); // true
+
+// Question: 51. Sort array of numbers
+
+// Ascending sort with compare function
+function sortNumbers(arr) {
+    // Numeric sort using subtraction
+    return arr.sort((a, b) => a - b);
+}
+console.log(sortNumbers([3, 1, 4, 2])); // [1, 2, 3, 4]
