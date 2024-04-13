5c5ffe93f8fccae2ef61ebfeb9b81b38dc1d57b2
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-04-14 00:00:00 +0530

    Added JavaScript practice question - Day 14

--- js-practice/day14.js
+++ js-practice/day14.js
@@ -13,3 +13,13 @@
     function isValidEmail(email) {
       return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
     }
+
+// Question: Convert callback to promise
+
+    function promisify(fn) {
+      return (...args) => new Promise((resolve, reject) => {
+        fn(...args, (err, result) => {
+          err ? reject(err) : resolve(result);
+        });
+      });
+    }
