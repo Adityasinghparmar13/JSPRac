d4b6a55db835e55715d91b22651f8d78a997cc92
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-04-17 00:00:00 +0530

    Added JavaScript practice question - Day 17

--- js-practice/day17.js
+++ js-practice/day17.js
@@ -15,3 +15,12 @@
       };
       return descriptor;
     }
+
+// Question: Implement Redux middleware
+
+    const loggerMiddleware = store => next => action => {
+      console.log('Dispatching:', action);
+      let result = next(action);
+      console.log('New state:', store.getState());
+      return result;
+    };
