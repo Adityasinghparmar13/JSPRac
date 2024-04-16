662653007a6136c32ae984988919a79043b2b731
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-04-17 00:00:00 +0530

    Added JavaScript practice question - Day 17

--- js-practice/day17.js
+++ js-practice/day17.js
@@ -4,3 +4,14 @@
     const MemoComponent = React.memo(({ prop }) => {
       return <div>{prop}</div>;
     });
+
+// Question: Create TypeScript decorator
+
+    function log(target, key, descriptor) {
+      const original = descriptor.value;
+      descriptor.value = function(...args) {
+        console.log('Called:', key);
+        return original.apply(this, args);
+      };
+      return descriptor;
+    }
