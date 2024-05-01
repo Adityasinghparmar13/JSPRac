ed681def18ab2bc410ebd9d9b046e29ded484489
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-05-02 00:00:00 +0530

    Added JavaScript practice question - Day 2

---
+++ js-practice/day2.js
@@ -0,0 +1,10 @@
+
+// Question: 6. Check prime number
+function isPrime(num) {
+    if (num <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+console.log(isPrime(7)); // true
