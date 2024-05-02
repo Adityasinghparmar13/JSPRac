bdc482932d1e9ea1d719b3d9da38e74919700091
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-05-03 00:00:00 +0530

    Added JavaScript practice question - Day 3

--- js-practice/day3.js
+++ js-practice/day3.js
@@ -7,3 +7,9 @@ function isLeapYear(year) {
     return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
 }
 console.log(isLeapYear(2020)); // true
+
+// Question: 9. Calculate array sum
+function arraySum(arr) {
+    return arr.reduce((acc, val) => acc + val, 0);
+}
+console.log(arraySum([1, 2, 3, 4])); // 10
