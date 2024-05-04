ae08cee14802a49248a68f52efcfc29b49df4952
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-05-05 00:00:00 +0530

    Added JavaScript practice question - Day 5

---
+++ js-practice/day5.js
@@ -0,0 +1,9 @@
+
+// Question: 18. Calculate rectangle area
+
+// Basic geometry calculation
+function rectangleArea(width, height) {
+    // Multiply dimensions
+    return width * height;
+}
+console.log(rectangleArea(5, 4)); // 20
