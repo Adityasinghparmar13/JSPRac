d55ce90c32f22bc0b8764d74f8e1f8cf33c16ecc
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-05-06 00:00:00 +0530

    Added JavaScript practice question - Day 6

--- js-practice/day6.js
+++ js-practice/day6.js
@@ -28,3 +28,12 @@ function toTitleCase(str) {
     ).join(' ');
 }
 console.log(toTitleCase('hello world')); // 'Hello World'
+
+// Question: 21. Calculate sphere volume
+
+// Calculate sphere volume using formula
+function sphereVolume(radius) {
+    // V = (4/3)πr³
+    return (4/3) * Math.PI * Math.pow(radius, 3);
+}
+console.log(sphereVolume(3).toFixed(2)); // 113.10
