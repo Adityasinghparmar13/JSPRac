713a40237b3ded2fda9267d4668965809be260d9
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-05-06 00:00:00 +0530

    Added JavaScript practice question - Day 6

--- js-practice/day6.js
+++ js-practice/day6.js
@@ -8,3 +8,12 @@ function countVowels(str) {
     return matches ? matches.length : 0;
 }
 console.log(countVowels('JavaScript')); // 3
+
+// Question: 22. Replace character in string
+
+// Replace all instances of a character
+function replaceChar(str, oldChar, newChar) {
+    // Use split and join method
+    return str.split(oldChar).join(newChar);
+}
+console.log(replaceChar('hello', 'l', 'x')); // 'hexxo'
