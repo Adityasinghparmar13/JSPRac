466aa94c3d30008604df51286e04d921336453ff
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-05-08 00:00:00 +0530

    Added JavaScript practice question - Day 8

--- js-practice/day8.js
+++ js-practice/day8.js
@@ -31,3 +31,12 @@ function sumDigits(num) {
     );
 }
 console.log(sumDigits(123)); // 6
+
+// Question: 29. Merge two arrays
+
+// Concatenate arrays without duplicates
+function mergeArrays(arr1, arr2) {
+    // Combine and filter duplicates
+    return [...new Set([...arr1, ...arr2])];
+}
+console.log(mergeArrays([1,2], [2,3])); // [1,2,3]
