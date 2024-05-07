4d124edaa998140f76a6624b01a9d13274252038
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-05-08 00:00:00 +0530

    Added JavaScript practice question - Day 8

--- js-practice/day8.js
+++ js-practice/day8.js
@@ -12,3 +12,12 @@ function fizzBuzz(n) {
     }
 }
 fizzBuzz(5); // 1, 2, Fizz, 4, Buzz
+
+// Question: 32. Calculate array average
+
+// Using reduce for sum calculation
+function arrayAverage(arr) {
+    // Sum elements and divide by length
+    return arr.reduce((a, b) => a + b, 0) / arr.length;
+}
+console.log(arrayAverage([4, 2, 8, 6])); // 5
