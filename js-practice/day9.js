05c471a006cf7ac70445b6fd237c17867abc9fc0
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-05-09 00:00:00 +0530

    Added JavaScript practice question - Day 9

--- js-practice/day9.js
+++ js-practice/day9.js
@@ -16,3 +16,12 @@ function arrayToString(arr, separator = ', ') {
     return arr.join(separator);
 }
 console.log(arrayToString(['a', 'b', 'c'], '-')); // 'a-b-c'
+
+// Question: 34. Extract object keys as array
+
+// Using Object.keys method
+function getObjectKeys(obj) {
+    // Return array of own property keys
+    return Object.keys(obj);
+}
+console.log(getObjectKeys({name: 'John', age: 30})); // ['name', 'age']
