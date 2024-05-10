ef9986440ac980dfd28eab1675b0eb529a52fe53
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-05-11 00:00:00 +0530

    Added JavaScript practice question - Day 11

--- js-practice/day11.js
+++ js-practice/day11.js
@@ -21,3 +21,14 @@ function capitalizeWords(str) {
     ).join(' ');
 }
 console.log(capitalizeWords('hello world')); // 'Hello World'
+
+// Question: 42. Flatten nested arrays
+
+// Recursive flattening using reduce
+function flattenArray(arr) {
+    return arr.reduce((acc, item) => {
+        // Handle nested arrays recursively
+        return acc.concat(Array.isArray(item) ? flattenArray(item) : item);
+    }, []);
+}
+console.log(flattenArray([1,[2,[3]]])); // [1,2,3]
