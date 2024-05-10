0878aaa53c14ce32d0e0edaaebb5626e40bdc386
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-05-11 00:00:00 +0530

    Added JavaScript practice question - Day 11

--- js-practice/day11.js
+++ js-practice/day11.js
@@ -32,3 +32,13 @@ function flattenArray(arr) {
     }, []);
 }
 console.log(flattenArray([1,[2,[3]]])); // [1,2,3]
+
+// Question: 43. Check array equality
+
+// Compare elements and length
+function arraysEqual(arr1, arr2) {
+    return arr1.length === arr2.length && 
+        // Verify each element matches
+        arr1.every((val, i) => val === arr2[i]);
+}
+console.log(arraysEqual([1,2], [1,2])); // true
