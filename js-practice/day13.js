311c4a3d65f92fa3c7bad8c42cfaace19a58de77
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-05-13 00:00:00 +0530

    Added JavaScript practice question - Day 13

--- js-practice/day13.js
+++ js-practice/day13.js
@@ -19,3 +19,13 @@ function areAnagrams(str1, str2) {
     return normalize(str1) === normalize(str2);
 }
 console.log(areAnagrams('listen', 'silent')); // true
+
+// Question: 52. Sum of squares
+
+// Square and sum using reduce
+function sumOfSquares(numbers) {
+    return numbers.reduce((sum, num) => 
+        sum + (num * num), 0
+    );
+}
+console.log(sumOfSquares([2, 3])); // 13
