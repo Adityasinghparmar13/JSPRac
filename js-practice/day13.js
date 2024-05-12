5c794ced21b35a2f41023c24b8ec8081002c62a5
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-05-13 00:00:00 +0530

    Added JavaScript practice question - Day 13

--- js-practice/day13.js
+++ js-practice/day13.js
@@ -29,3 +29,12 @@ function sumOfSquares(numbers) {
     );
 }
 console.log(sumOfSquares([2, 3])); // 13
+
+// Question: 51. Sort array of numbers
+
+// Ascending sort with compare function
+function sortNumbers(arr) {
+    // Numeric sort using subtraction
+    return arr.sort((a, b) => a - b);
+}
+console.log(sortNumbers([3, 1, 4, 2])); // [1, 2, 3, 4]
