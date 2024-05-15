520611a8f36b4894c18603eff63206dcbf98602a
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-05-16 00:00:00 +0530

    Added JavaScript practice question - Day 16

--- js-practice/day16.js
+++ js-practice/day16.js
@@ -46,3 +46,31 @@ function deepClone(obj) {
 // Test case:
 const original = { a: [1, { b: 2 }] };
 const cloned = deepClone(original);
+
+// Question: 4. Promise concurrency pool
+```javascript
+// Control parallel promise execution
+async function promisePool(tasks, poolSize) {
+    const results = [];
+    const executing = [];
+    
+    for (const task of tasks) {
+        // Create promise and track execution
+        const p = Promise.resolve().then(task);
+        results.push(p);
+        
+        // Manage pool size
+        const e = p.then(() => executing.splice(executing.indexOf(e), 1));
+        executing.push(e);
+        
+        // Wait when pool is full
+        if (executing.length >= poolSize) {
+            await Promise.race(executing);
+        }
+    }
+    return Promise.all(results);
+}
+
+// Usage:
+const tasks = [/* array of async functions */];
+promisePool(tasks, 3);
