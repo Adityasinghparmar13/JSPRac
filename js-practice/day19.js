e7ca1d45633dcc261df13cd876fe3a205ef1dd06
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-05-19 00:00:00 +0530

    Added JavaScript practice question - Day 19

--- js-practice/day19.js
+++ js-practice/day19.js
@@ -40,3 +40,25 @@ class PubSub {
         this.subscribers[topic] = (this.subscribers[topic] || []).filter(fn => fn !== callback);
     }
 }
+
+// Question: 13. Compose middleware functions
+```javascript
+function compose(middlewares) {
+    return (context, next) => {
+        let index = -1;
+        const dispatch = (i) => {
+            if (i <= index) return Promise.reject();
+            index = i;
+            let fn = middlewares[i] || next;
+            if (!fn) return Promise.resolve();
+            try {
+                return Promise.resolve(
+                    fn(context, () => dispatch(i + 1))
+                );
+            } catch (err) {
+                return Promise.reject(err);
+            }
+        };
+        return dispatch(0);
+    };
+}
