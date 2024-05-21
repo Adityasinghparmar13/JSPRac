6216b4a6e705e04b25e821354eb0e6e0883eb769
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-05-22 00:00:00 +0530

    Added JavaScript practice question - Day 22

--- js-practice/day22.js
+++ js-practice/day22.js
@@ -64,3 +64,24 @@ class CircularBuffer {
         return item;
     }
 }
+
+// Question: 27. Graph BFS traversal
+```javascript
+function bfs(graph, start) {
+    const visited = new Set();
+    const queue = [start];
+    const result = [];
+    
+    while (queue.length) {
+        const node = queue.shift();
+        if (!visited.has(node)) {
+            visited.add(node);
+            result.push(node);
+            queue.push(...graph[node].filter(n => !visited.has(n)));
+        }
+    }
+    return result;
+}
+
+// Adjacency list format:
+const graph = { A: ['B', 'C'], B: ['D'], C: [], D: [] };
