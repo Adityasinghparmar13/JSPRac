9a7f9f4a1f9059603b91c83fe3da816478032070
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-05-22 00:00:00 +0530

    Added JavaScript practice question - Day 22

--- js-practice/day22.js
+++ js-practice/day22.js
@@ -39,3 +39,28 @@ class Trie {
         node.isEnd = true;
     }
 }
+
+// Question: 26. Circular buffer implementation
+```javascript
+class CircularBuffer {
+    constructor(capacity) {
+        this.buffer = new Array(capacity);
+        this.head = 0;
+        this.tail = 0;
+        this.size = 0;
+    }
+
+    push(item) {
+        this.buffer[this.tail] = item;
+        this.tail = (this.tail + 1) % this.buffer.length;
+        this.size = Math.min(this.size + 1, this.buffer.length);
+    }
+
+    pop() {
+        if (this.size === 0) return null;
+        const item = this.buffer[this.head];
+        this.head = (this.head + 1) % this.buffer.length;
+        this.size--;
+        return item;
+    }
+}
