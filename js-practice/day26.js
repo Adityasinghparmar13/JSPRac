4d64c0e82e6bb787b7ff68bde9ba05b5fef9bc0e
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-05-26 00:00:00 +0530

    Added JavaScript practice question - Day 26

--- js-practice/day26.js
+++ js-practice/day26.js
@@ -16,3 +16,20 @@ class Service {
 // Usage:
 const service = new Service();
 service.log('Initialized'); // Timestamped message
+
+// Question: 42. Memoization with WeakMap
+```javascript
+function weakMemoize(fn) {
+    const cache = new WeakMap();
+    return (obj) => {
+        if (!cache.has(obj)) {
+            cache.set(obj, fn(obj));
+        }
+        return cache.get(obj);
+    };
+}
+
+// Usage:
+const memoizedObjProcessor = weakMemoize(obj => {
+    return Object.keys(obj).join(',');
+});
