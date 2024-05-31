0df8ec69f9aa288f452f22f34d249034b9592e11
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-06-01 00:00:00 +0530

    Added JavaScript practice question - Day 1

--- js-practice/day1.js
+++ js-practice/day1.js
@@ -14,3 +14,8 @@ function factorial(n) {
     return result;
 }
 console.log(factorial(5)); // 120
+
+// Question: 1. Declare a variable and print it
+// Declare variable using let keyword
+let greeting = 'Hello, World!';
+console.log(greeting);
