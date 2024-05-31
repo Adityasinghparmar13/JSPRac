52057a3f8fff2f32d5790c8b5f98ff4314ca2d66
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-06-01 00:00:00 +0530

    Added JavaScript practice question - Day 1

--- js-practice/day1.js
+++ js-practice/day1.js
@@ -19,3 +19,9 @@ console.log(factorial(5)); // 120
 // Declare variable using let keyword
 let greeting = 'Hello, World!';
 console.log(greeting);
+
+// Question: 2. Add two numbers
+function add(a, b) {
+    return a + b;
+}
+console.log(add(5, 3)); // 8
