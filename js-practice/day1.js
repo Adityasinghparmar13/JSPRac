3624a752bf292bbce47a205f102b3ad5c96b68fc
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-06-01 00:00:00 +0530

    Added JavaScript practice question - Day 1

--- js-practice/day1.js
+++ js-practice/day1.js
@@ -1,23 +1,6 @@
 
-// Question: 3. Check even or odd
-function isEven(num) {
-    return num % 2 === 0;
-}
-console.log(isEven(4)); // true
-
-// Question: 1. Declare a variable and print it
-// Declare variable using let keyword
-let greeting = 'Hello, World!';
-console.log(greeting);
-
 // Question: 4. Find largest of three numbers
 function findLargest(a, b, c) {
     return Math.max(a, b, c);
 }
 console.log(findLargest(5, 9, 3)); // 9
-
-// Question: 2. Add two numbers
-function add(a, b) {
-    return a + b;
-}
-console.log(add(5, 3)); // 8
