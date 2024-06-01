0866a490e79990ddc510603d540f4afe12164964
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-06-02 00:00:00 +0530

    Added JavaScript practice question - Day 2

--- js-practice/day2.js
+++ js-practice/day2.js
@@ -35,3 +35,10 @@ function isPrime(num) {
     return true;
 }
 console.log(isPrime(7)); // true
+
+// Question: 8. Check palindrome
+function isPalindrome(str) {
+    const cleaned = str.toLowerCase();
+    return cleaned === reverseString(cleaned);
+}
+console.log(isPalindrome('Madam')); // true
