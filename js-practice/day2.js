ee27c76b36c3b49a27bc4f67856587b9ea925454
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-06-02 00:00:00 +0530

    Added JavaScript practice question - Day 2

--- js-practice/day2.js
+++ js-practice/day2.js
@@ -10,3 +10,12 @@ function arraySum(arr) {
     return arr.reduce((acc, val) => acc + val, 0);
 }
 console.log(arraySum([1, 2, 3, 4])); // 10
+
+// Question: 11. Celsius to Fahrenheit conversion
+
+// Conversion formula implementation
+function celsiusToFahrenheit(c) {
+    // (C × 9/5) + 32
+    return (c * 9/5) + 32;
+}
+console.log(celsiusToFahrenheit(25)); // 77
