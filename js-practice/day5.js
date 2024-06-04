ea4b8eec9e0fed2eb558a3d950f0bf658f981d4d
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-06-05 00:00:00 +0530

    Added JavaScript practice question - Day 5

--- js-practice/day5.js
+++ js-practice/day5.js
@@ -8,3 +8,12 @@ function countVowels(str) {
     return matches ? matches.length : 0;
 }
 console.log(countVowels('JavaScript')); // 3
+
+// Question: 21. Calculate sphere volume
+
+// Calculate sphere volume using formula
+function sphereVolume(radius) {
+    // V = (4/3)πr³
+    return (4/3) * Math.PI * Math.pow(radius, 3);
+}
+console.log(sphereVolume(3).toFixed(2)); // 113.10
