e59eb600cdc95c1d7f6273a8fa52ce5a8c4308bc
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-06-05 00:00:00 +0530

    Added JavaScript practice question - Day 5

--- js-practice/day5.js
+++ js-practice/day5.js
@@ -17,3 +17,12 @@ function sphereVolume(radius) {
     return (4/3) * Math.PI * Math.pow(radius, 3);
 }
 console.log(sphereVolume(3).toFixed(2)); // 113.10
+
+// Question: 22. Replace character in string
+
+// Replace all instances of a character
+function replaceChar(str, oldChar, newChar) {
+    // Use split and join method
+    return str.split(oldChar).join(newChar);
+}
+console.log(replaceChar('hello', 'l', 'x')); // 'hexxo'
