b608cf4da6a3a3a07d08519cd9ff90f8f7303e8b
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-06-06 00:00:00 +0530

    Added JavaScript practice question - Day 6

--- js-practice/day6.js
+++ js-practice/day6.js
@@ -32,3 +32,12 @@ function capitalize(str) {
         : '';
 }
 console.log(capitalize('test')); // 'Test'
+
+// Question: 29. Merge two arrays
+
+// Concatenate arrays without duplicates
+function mergeArrays(arr1, arr2) {
+    // Combine and filter duplicates
+    return [...new Set([...arr1, ...arr2])];
+}
+console.log(mergeArrays([1,2], [2,3])); // [1,2,3]
