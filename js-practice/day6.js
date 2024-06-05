36592969a90c31646d8ec4d2b76be12ef30eb54c
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-06-06 00:00:00 +0530

    Added JavaScript practice question - Day 6

--- js-practice/day6.js
+++ js-practice/day6.js
@@ -8,3 +8,16 @@ function sumDigits(num) {
     );
 }
 console.log(sumDigits(123)); // 6
+
+// Question: 28. Reverse array elements
+
+// Manual array reversal
+function reverseArray(arr) {
+    const reversed = [];
+    // Iterate from end to start
+    for (let i = arr.length - 1; i >= 0; i--) {
+        reversed.push(arr[i]);
+    }
+    return reversed;
+}
+console.log(reverseArray([1,2,3])); // [3,2,1]
