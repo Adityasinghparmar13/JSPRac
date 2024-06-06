28078a69e2d78968cccba80255b1f684085e6f06
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-06-07 00:00:00 +0530

    Added JavaScript practice question - Day 7

--- js-practice/day7.js
+++ js-practice/day7.js
@@ -43,3 +43,17 @@ function stringToWordArray(str) {
     return str.split(/\s+/);
 }
 console.log(stringToWordArray('Hello world JS')); // ['Hello', 'world', 'JS']
+
+// Question: 31. FizzBuzz implementation
+
+// Classic FizzBuzz logic with modulus
+function fizzBuzz(n) {
+    for (let i = 1; i <= n; i++) {
+        let output = '';
+        // Check divisibility rules
+        if (i % 3 === 0) output += 'Fizz';
+        if (i % 5 === 0) output += 'Buzz';
+        console.log(output || i); // Default to number if empty
+    }
+}
+fizzBuzz(5); // 1, 2, Fizz, 4, Buzz
