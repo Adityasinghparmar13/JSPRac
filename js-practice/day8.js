596ec3e31d8251f72c373f4a827072ab321be787
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-06-08 00:00:00 +0530

    Added JavaScript practice question - Day 8

--- js-practice/day8.js
+++ js-practice/day8.js
@@ -10,3 +10,15 @@ function findIndex(arr, element) {
     return -1;
 }
 console.log(findIndex(['a', 'b', 'c'], 'b')); // 1
+
+// Question: 41. Count element occurrences
+
+// Create frequency counter using reduce
+function countOccurrences(arr) {
+    return arr.reduce((acc, val) => {
+        // Increment count for each element
+        acc[val] = (acc[val] || 0) + 1;
+        return acc;
+    }, {});
+}
+console.log(countOccurrences(['a','b','a','c'])); // {a:2, b:1, c:1}
