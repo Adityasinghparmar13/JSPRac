7812c01ec1f716aaac51bddffb20fe62ac42b7dd
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-06-08 00:00:00 +0530

    Added JavaScript practice question - Day 8

--- js-practice/day8.js
+++ js-practice/day8.js
@@ -22,3 +22,12 @@ function countOccurrences(arr) {
     }, {});
 }
 console.log(countOccurrences(['a','b','a','c'])); // {a:2, b:1, c:1}
+
+// Question: 38. Filter array elements
+
+// Custom filter for even numbers
+function filterEvens(numbers) {
+    // Return new array with even numbers
+    return numbers.filter(num => num % 2 === 0);
+}
+console.log(filterEvens([1, 2, 3, 4])); // [2, 4]
