421a75961be61746ebdd20c2f23c06d2288675b2
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-06-09 00:00:00 +0530

    Added JavaScript practice question - Day 9

--- js-practice/day9.js
+++ js-practice/day9.js
@@ -8,3 +8,14 @@ function arraysEqual(arr1, arr2) {
         arr1.every((val, i) => val === arr2[i]);
 }
 console.log(arraysEqual([1,2], [1,2])); // true
+
+// Question: 42. Flatten nested arrays
+
+// Recursive flattening using reduce
+function flattenArray(arr) {
+    return arr.reduce((acc, item) => {
+        // Handle nested arrays recursively
+        return acc.concat(Array.isArray(item) ? flattenArray(item) : item);
+    }, []);
+}
+console.log(flattenArray([1,[2,[3]]])); // [1,2,3]
