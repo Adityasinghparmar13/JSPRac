a35a183f326dc416c926566c7cd81f9b194c2cc8
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-06-09 00:00:00 +0530

    Added JavaScript practice question - Day 9

---
+++ js-practice/day9.js
@@ -0,0 +1,10 @@
+
+// Question: 43. Check array equality
+
+// Compare elements and length
+function arraysEqual(arr1, arr2) {
+    return arr1.length === arr2.length && 
+        // Verify each element matches
+        arr1.every((val, i) => val === arr2[i]);
+}
+console.log(arraysEqual([1,2], [1,2])); // true
