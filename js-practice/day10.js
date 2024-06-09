722dd2f61f7995e756ee902c0ecf2f622114f753
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-06-10 00:00:00 +0530

    Added JavaScript practice question - Day 10

--- js-practice/day10.js
+++ js-practice/day10.js
@@ -7,3 +7,14 @@ function sortNumbers(arr) {
     return arr.sort((a, b) => a - b);
 }
 console.log(sortNumbers([3, 1, 4, 2])); // [1, 2, 3, 4]
+
+// Question: 48. Truncate string with ellipsis
+
+// Limit string length with truncation
+function truncateString(str, maxLength) {
+    // Add ellipsis if truncated
+    return str.length > maxLength 
+        ? str.slice(0, maxLength) + '...' 
+        : str;
+}
+console.log(truncateString('Long text here', 8)); // 'Long tex...'
