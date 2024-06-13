23fbedda486dd7bc3d874e92511b4a6daae53779
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-06-14 00:00:00 +0530

    Added JavaScript practice question - Day 14

--- js-practice/day14.js
+++ js-practice/day14.js
@@ -127,3 +127,31 @@ function binarySearch(arr, target) {
 
 // Test case:
 console.log(binarySearch([1,3,5,7,9], 5)); // 2
+
+// Question: 4. Promise concurrency pool
+```javascript
+// Control parallel promise execution
+async function promisePool(tasks, poolSize) {
+    const results = [];
+    const executing = [];
+    
+    for (const task of tasks) {
+        // Create promise and track execution
+        const p = Promise.resolve().then(task);
+        results.push(p);
+        
+        // Manage pool size
+        const e = p.then(() => executing.splice(executing.indexOf(e), 1));
+        executing.push(e);
+        
+        // Wait when pool is full
+        if (executing.length >= poolSize) {
+            await Promise.race(executing);
+        }
+    }
+    return Promise.all(results);
+}
+
+// Usage:
+const tasks = [/* array of async functions */];
+promisePool(tasks, 3);
