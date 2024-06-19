3b3836d56988e9d4a326a034cd1a84ec83bf9f36
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-06-20 00:00:00 +0530

    Added JavaScript practice question - Day 20

--- js-practice/day20.js
+++ js-practice/day20.js
@@ -28,3 +28,23 @@ class ObservableStream {
 const map = f => source => new ObservableStream(emit => {
     source.subscribe(val => emit(f(val)));
 };
+
+// Question: 35. Abortable timeout promise
+```javascript
+function abortableTimeout(delay) {
+    const controller = new AbortController();
+    const timeout = new Promise((_, reject) => {
+        const timer = setTimeout(() => {
+            reject(new Error('Timeout'));
+        }, delay);
+        controller.signal.addEventListener('abort', () => {
+            clearTimeout(timer);
+            reject(new Error('Aborted'));
+        });
+    });
+    return { timeout, abort: () => controller.abort() };
+}
+
+// Usage:
+const { timeout, abort } = abortableTimeout(5000);
+timeout.catch(console.error);
