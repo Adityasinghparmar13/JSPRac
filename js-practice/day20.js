97a6960d3a1060ac3d31d2ad3026ad19266fbf29
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-06-20 00:00:00 +0530

    Added JavaScript practice question - Day 20

--- js-practice/day20.js
+++ js-practice/day20.js
@@ -69,3 +69,26 @@ class ImmutableList {
         return [...this._data];
     }
 }
+
+// Question: 33. Async middleware chain with context
+```javascript
+async function middlewareChain(context, middlewares) {
+    let index = 0;
+    const next = async () => {
+        if (index < middlewares.length) {
+            return middlewares[index++](context, next);
+        }
+    };
+    return next();
+}
+
+// Usage:
+middlewareChain({}, [
+    async (ctx, next) => {
+        ctx.user = await fetchUser();
+        await next();
+    },
+    async (ctx) => {
+        console.log(ctx.user);
+    }
+]);
