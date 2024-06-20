2a5c95caad0031781a2ea383fc67b0cb92ba288a
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-06-21 00:00:00 +0530

    Added JavaScript practice question - Day 21

--- js-practice/day21.js
+++ js-practice/day21.js
@@ -86,3 +86,29 @@ function parseTemplate(str, data) {
 
 // Usage:
 parseTemplate('Hello ${name}!', { name: 'Alice' }); // 'Hello Alice!'
+
+// Question: 38. Observable async iterator
+```javascript
+function createAsyncIterable() {
+    const queue = [];
+    let resolveNext;
+    
+    return {
+        async *[Symbol.asyncIterator]() {
+            while (true) {
+                if (queue.length) yield queue.shift();
+                else await new Promise(r => resolveNext = r);
+            }
+        },
+        push(value) {
+            queue.push(value);
+            if (resolveNext) {
+                resolveNext();
+                resolveNext = null;
+            }
+        }
+    };
+}
+
+// Usage:
+const asyncIterable = createAsyncIterable();
