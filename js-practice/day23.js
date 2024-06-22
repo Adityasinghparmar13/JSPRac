d79fdee8e2203bba9d02e7fb4c673501703bf8b9
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-06-23 00:00:00 +0530

    Added JavaScript practice question - Day 23

--- js-practice/day23.js
+++ js-practice/day23.js
@@ -50,3 +50,30 @@ class HandlerChain {
 const chain = new HandlerChain()
     .addHandler(req => req < 10 ? 'Handled by first' : null)
     .addHandler(req => req < 100 ? 'Handled by second' : null);
+
+// Question: 52. Tree data structure implementation
+```javascript
+class TreeNode {
+    constructor(value) {
+        this.value = value;
+        this.children = [];
+    }
+
+    addChild(node) {
+        this.children.push(node);
+    }
+
+    traverseDFS(cb) {
+        cb(this.value);
+        this.children.forEach(child => child.traverseDFS(cb));
+    }
+
+    traverseBFS(cb) {
+        const queue = [this];
+        while (queue.length) {
+            const node = queue.shift();
+            cb(node.value);
+            queue.push(...node.children);
+        }
+    }
+}
