74a35a7ad089a34e4f9c9c21c9a25d8fcaaa6daa
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-09-03 00:00:00 +0530

    Added JavaScript practice question - Day 3

--- js-practice/day3.js
+++ js-practice/day3.js
@@ -19,3 +19,12 @@ function celsiusToFahrenheit(c) {
     return (c * 9/5) + 32;
 }
 console.log(celsiusToFahrenheit(25)); // 77
+
+// Question: 12. Check leap year
+
+// Leap year logic with multiple conditions
+function isLeapYear(year) {
+    // Divisible by 4 but not 100 unless also 400
+    return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+}
+console.log(isLeapYear(2020)); // true
