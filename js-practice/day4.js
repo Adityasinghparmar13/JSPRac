fed476dd2c9d72f3f98375e6139555bbf0479b09
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-09-04 00:00:00 +0530

    Added JavaScript practice question - Day 4

---
+++ js-practice/day4.js
@@ -0,0 +1,9 @@
+
+// Question: 13. Simple interest calculator
+
+// Mathematical formula implementation
+function simpleInterest(p, r, t) {
+    // SI = (P × R × T)/100
+    return (p * r * t) / 100;
+}
+console.log(simpleInterest(1000, 5, 2)); // 100
