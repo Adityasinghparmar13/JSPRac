9250dbf4373543a7c009243e64c6b1544aef3acb
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2025-02-02 00:00:00 +0530

    Added JavaScript practice question - Day 2

--- js-practice/day2.js
+++ js-practice/day2.js
@@ -24,3 +24,10 @@ function reverseString(str) {
     return str.split('').reverse().join('');
 }
 console.log(reverseString('hello')); // 'olleh'
+
+// Question: 8. Check palindrome
+function isPalindrome(str) {
+    const cleaned = str.toLowerCase();
+    return cleaned === reverseString(cleaned);
+}
+console.log(isPalindrome('Madam')); // true
